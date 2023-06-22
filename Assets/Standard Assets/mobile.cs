089b41b2fdf1c4967a329261c1b9653c281e2307
Hady97-dev <ahmedhandasa97@outlook.com>
2023-06-23 00:25:47 +0200

    added animation and sound and player script

---
+++ Assets/Standard Assets/mobile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mobile : MonoBehaviour {
+
+	// Use this for initialization
+
+	public float moveSpeed = 15f;
+	public float CountUp=0f ;
+
+	
+
+
+
+	 public void Right()
+	{
+		
+			transform.Translate (-Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+
+	public void Left()
+	{
+		 
+
+			transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+
+		void FixedUpdate()
+	{
+		CountUp += (Time.deltaTime);
+		if (CountUp >= 5.0f) {
+
+			moveSpeed = (moveSpeed + 0.001f);
+
+		}
+
+		}
+}
--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -5,28 +5,72 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float Speed;
+    public Animator anim;
+    public Rigidbody rb;
+    public GameObject ShowButton;
+  
+    public GameObject AnimCamera;
+    public float AnimCamAngle;
 
-    public string Name;
-    public string Description;
-    public Sprite PlayerImage;                         //if you use public image ...will not work 
-    public PlayerHealthAndMana healthAndMana;
-    public PlayerMovement playerMovement;
-    public PlayerAnimator playerAnimator;
-    public PlayerAbilities playerAbilities;
-    public PlayerCombat playerCombat;
-    
+    public AudioSource attackSound;
+    public AudioSource JumpingSound;
 
     public void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    public void Update()
+    public void FixedUpdate()
     {
-
+          
+        if (Input.GetKey(KeyCode.Space) )
+        {
+            Attack();
+        }
+       
     }
 
+    IEnumerator AttackDuration(int secs)
+    {
+        yield return new WaitForSeconds(secs);
+        anim.SetBool("IsAttacking", false);
 
+    }
+    IEnumerator JumpingDuration(int secs)
+    {
+        yield return new WaitForSeconds(secs);
+        anim.SetBool("IsJumping", false);
+        AnimCamera.SetActive(false);
+    }
+
+    public void Attack()
+    {
+        StartCoroutine(AttackDuration(2));
+        anim.SetBool("IsAttacking", true);
+        attackSound.Play();
+    }
+
+    public void AttackAndJump()
+    {
+        StartCoroutine(JumpingDuration(3));
+        anim.SetBool("IsJumping", true);
+        AnimCamera.SetActive(true);
+        AnimCamera.transform.RotateAround(gameObject.transform.position, transform.position, AnimCamAngle);
+        JumpingSound.Play();
+    }
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+        ShowButton.SetActive(true);
+    }
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        ShowButton.SetActive(false);
+
+    }
 
 
 
