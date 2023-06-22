1dc12c790741e382bfd402344dcfcae946d55179
Hady97-dev <ahmedhandasa97@outlook.com>
2023-06-22 17:49:29 +0200

    Creating new project

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+
+    public string Name;
+    public string Description;
+    public Sprite PlayerImage;                         //if you use public image ...will not work 
+    public PlayerHealthAndMana healthAndMana;
+    public PlayerMovement playerMovement;
+    public PlayerAnimator playerAnimator;
+    public PlayerAbilities playerAbilities;
+    public PlayerCombat playerCombat;
+    
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+
+    }
+
+
+
+
+
+}
